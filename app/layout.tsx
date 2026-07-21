38dc0ba09c67f9540619f95bcd6e182b171804fc
Giliard Gomes <giliard.gomes59@gmail.com>
2026-07-21 12:27:02 -0300

    Integrate Google Analytics tracking script
    Added Google Analytics tracking script to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R9F1TESR2K"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-R9F1TESR2K');
+        </script>
         <script
           suppressHydrationWarning
           dangerouslySetInnerHTML={{
